814d45a03dba5c7f0758c18c106fb0b82ce5b821
José Antonio das Neves Neto <ef128@uol.com.br>
2022-10-06 17:07:56 -0300

    Criação do modelo de dados para base de dados do SSO.

---
+++ DELFOS.JWT.SSO.API/Models/AppPortifolio.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DELFOS.JWT.SSO.API.Models
+{
+    public class AppPortifolio
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public string? ApplicationName { get; set; }
+
+        public string? Status { get; set; } 
+
+        public OrganizationModel? Organization { get; set; }
+        public UserModel? User { get; set; }    
+
+    }
+}
---
+++ DELFOS.JWT.SSO.API/Models/OrganizationModel.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace DELFOS.JWT.SSO.API.Models
+{
+    public class OrganizationModel
+    {
+        [Key, Column(Order = 0)]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required, Column(Order =1), StringLength(150)]
+        public string? Razao { get; set; }
+
+        [Required, Column(Order =2), StringLength(15)]
+        public string?CNPJ { get; set; }
+
+        [Required,Column(Order =3)]
+        public bool? Status { get; set; }
+
+        [Column(Order =4), StringLength(150)]
+        public string? Address { get; set; }
+
+        [Column(Order =5), StringLength(5)]
+        public string? NumberAddr { get; set; }
+
+        [Column(Order =6), StringLength(9)]
+        public string? ZipCode { get; set; }
+
+        [Column(Order =7), StringLength(50)]
+        public string? District { get; set; } //bairro
+
+        [Column(Order =8), StringLength(100)]
+        public string? City { get; set; }
+
+        [Column(Order =9)]
+        public int StateId { get; set; }
+
+        [Column(Order =10)]
+        public int CountryId { get; set; }
+
+        public CountryModel? Country { get; set; } 
+    }
+}
